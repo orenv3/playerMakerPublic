32f6d9f29ba77d9eeb0c65ca661fdd1a3c9f30a8
oren vinogura <vinogura.oren@gmail.com>
2022-02-06 11:28:15 +0200

    C:/Program Files/Git/PlayerMakerConfiguration.java

--- src/main/java/com/playermaker/config/PlayerMakerConfiguration.java
+++ src/main/java/com/playermaker/config/PlayerMakerConfiguration.java
@@ -1,6 +1,5 @@
 package com.playermaker.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -10,9 +9,5 @@ import com.playermaker.errorsHandler.GlobalExceptionHandler;
 @Import(GlobalExceptionHandler.class)
 public class PlayerMakerConfiguration {
 
-//	@Bean
-//	public IpFiltering ipfltering() {
-//		return new IpFiltering();
-//	}
 	
 }
